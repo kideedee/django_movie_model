a8eb9d8355ae326803faae19641d6a00c93b2502
sonvq5 <kideedee1234@gmail.com>
2023-08-11 10:30:59 +0700

    adding api endpoints

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from movies.models import MovieData
+from movies.serializers import MovieSerializer
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = MovieData.objects.all()
+    serializer_class = MovieSerializer
